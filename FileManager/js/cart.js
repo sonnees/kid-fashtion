03a1eca13f7d4af3a327eef6b0ecd25bcd1935e5
sonnees <sonnees.contact@gmail.com>
2022-10-29 14:20:38 +0700

    cart-định dạng tiền phần ngàn

--- FileManager/js/cart.js
+++ FileManager/js/cart.js
@@ -19,6 +19,8 @@ function updateTotal() {
     var QTY2 =  document.getElementById("input2").value;
     var QTY3 =  document.getElementById("input3").value;
     total= gia1*parseInt(QTY1)+gia2*parseInt(QTY2)+gia3*parseInt(QTY3)
-    document.getElementById('txtTotal').innerHTML = total;
+    var totalString = total.toString(). 
+            replace(/\B(?=(\d{3})+(?!\d))/g,".");
+    document.getElementById('txtTotal').innerHTML = totalString;
 }
 
